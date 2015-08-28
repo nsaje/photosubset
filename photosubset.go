0775c9b1f488754aab02968ed32e327513ab9c5d
Nejc Saje <nejc@saje.info>
2015-08-28 10:08:34 +0200

    initial version

---
+++ photosubset.go
@@ -0,0 +1,182 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"gopkg.in/qml.v1"
+)
+
+var dir string
+var search string
+
+func main() {
+	if len(os.Args) < 2 {
+		os.Exit(1)
+	}
+	dir = os.Args[1]
+	if len(os.Args) > 2 {
+		search = os.Args[2]
+	}
+
+	if err := qml.Run(run); err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
+
+func run() error {
+	controller := NewPhotoController(".")
+
+	engine := qml.NewEngine()
+	engine.Context().SetVar("controller", controller)
+
+	component, err := engine.LoadFile("qrc:///qml/photosubset.qml")
+	if err != nil {
+		return err
+	}
+
+	win := component.CreateWindow(nil)
+	// win.Set("visibility", 5) // fullscreen
+	win.Show()
+	win.Wait()
+
+	return nil
+}
+
+func NewPhotoController(path string) *PhotoController {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	images := make([]string, 0, len(files))
+	for _, f := range files {
+		if isImage(f.Name()) {
+			absPath, _ := filepath.Abs(f.Name())
+			images = append(images, "file://"+absPath)
+		}
+	}
+	index := 0
+	if len(search) > 0 {
+		for i, name := range images {
+			if strings.Contains(filepath.Base(name), search) {
+				index = i
+				break
+			}
+		}
+	}
+	pc := &PhotoController{files: images, index: index}
+	pc.changePhoto()
+	return pc
+}
+
+// PhotoController takes care of image browsing and tagging
+type PhotoController struct {
+	files []string
+	index int
+
+	CurrentPhotoPath string
+	TagsText         string
+	currentTagNames  [10]string
+	currentTags      [10]bool
+}
+
+func isImage(path string) bool {
+	f, err := os.Open(path)
+	if err != nil {
+		return false
+	}
+	b := make([]byte, 512)
+	_, err = f.Read(b)
+	if err != nil {
+		return false
+	}
+	contentType := http.DetectContentType(b)
+	return strings.HasPrefix(contentType, "image/")
+}
+
+func (c *PhotoController) changePhoto() {
+	c.CurrentPhotoPath = c.files[c.index]
+	qml.Changed(c, &c.CurrentPhotoPath)
+	c.getTags()
+}
+
+func (c *PhotoController) getTags() {
+	c.currentTagNames = [10]string{}
+	c.currentTags = [10]bool{}
+	files, err := ioutil.ReadDir("./tags")
+	if err != nil {
+		return
+	}
+
+	imageName := filepath.Base(c.CurrentPhotoPath)
+	for _, f := range files {
+		if f.IsDir() && strings.HasPrefix(f.Name(), "tag-") {
+			i, err := strconv.Atoi(string(f.Name()[4]))
+			if err != nil {
+				return
+			}
+			c.currentTagNames[i] = f.Name()
+
+			taggedPath := filepath.Join("./tags", f.Name(), imageName)
+			if _, err := os.Stat(taggedPath); err == nil {
+				c.currentTags[i] = true
+			}
+		}
+	}
+	fmt.Println(c.currentTagNames)
+	fmt.Println(c.currentTags)
+
+	c.TagsText = ""
+	for i, tagged := range c.currentTags {
+		if tagged {
+			c.TagsText = c.TagsText + fmt.Sprintln(c.currentTagNames[i][4:])
+		}
+	}
+	qml.Changed(c, &c.TagsText)
+}
+
+func (c *PhotoController) Next() {
+	if c.index < len(c.files)-1 {
+		c.index++
+		c.changePhoto()
+	}
+}
+
+func (c *PhotoController) Prev() {
+	if c.index > 0 {
+		c.index--
+		c.changePhoto()
+	}
+}
+
+func (c *PhotoController) Tag(num int) {
+	imageName := filepath.Base(c.CurrentPhotoPath)
+	tagName := c.currentTagNames[num]
+	if len(tagName) == 0 {
+		tagName = fmt.Sprintf("tag-%d", num)
+		os.Mkdir(filepath.Join("./tags", tagName), 0777)
+	}
+	taggedPath := filepath.Join("./tags", tagName, imageName)
+	if c.currentTags[num] {
+		err := os.Remove(taggedPath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	} else {
+		err := os.Link(imageName, taggedPath)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+	c.getTags()
+}
---
+++ qml/photosubset.qml
@@ -0,0 +1,57 @@
+import QtQuick 2.5
+import QtQuick.Window 2.0
+
+Rectangle {
+    width: Screen.width
+    height: Screen.height
+    focus: true
+
+		Keys.onLeftPressed: {
+			console.log("left pressed");
+			controller.prev();
+			event.accepted = true;
+		}
+		Keys.onRightPressed: {
+			console.log("right pressed");
+			controller.next();
+			event.accepted = true;
+		}
+		Keys.onPressed: {
+			if (event.key >= 0x30 && event.key <= 0x39) {
+        var num = event.key - 0x30;
+        console.log("digit " + num + " pressed");
+				controller.tag(num);
+        event.accepted = true;
+			}
+		}
+
+		Image {
+			id: imageContainer
+			source: controller.currentPhotoPath
+			fillMode: Image.PreserveAspectFit
+      width: parent.width
+      height: parent.height
+      sourceSize.width: parent.width
+      autoTransform: true
+      anchors.horizontalCenter: parent.horizontalCenter
+      anchors.verticalCenter: parent.verticalCenter
+		}
+
+    Text {
+      id: tagsText
+      text: controller.tagsText
+      font.pointSize: 34
+    }
+
+    Binding {
+      target: imageContainer
+      property: "source"
+      value: controller.currentPhotoPath
+    }
+
+    Binding {
+      target: tagsText
+      property: "text"
+      value: controller.tagsText
+    }
+}
